efd7b9fbdc5f4151a465c0a977250a5efefb322e
jaggesher <jaggesher14@gmail.com>
2018-04-06 18:06:54 +0600

    admin and shopper module added

---
+++ src/app/admin-module/admin-module.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AdminModuleRoutingModule } from './admin-module-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    AdminModuleRoutingModule
+  ],
+  declarations: []
+})
+export class AdminModuleModule { }
---
+++ src/app/shopper-module/shopper-module.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ShopperModuleRoutingModule } from './shopper-module-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    ShopperModuleRoutingModule
+  ],
+  declarations: []
+})
+export class ShopperModuleModule { }
