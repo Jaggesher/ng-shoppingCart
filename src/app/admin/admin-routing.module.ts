0c93724c6daf002c7ad7e1fe334d99a601c98952
jaggesher <jaggesher14@gmail.com>
2018-04-09 01:38:00 +0600

    Admin New Product Component added

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,10 +1,13 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import { NewProductComponent } from './new-product/new-product.component';
+const routes: Routes = [
+  {path: 'AddProduct' , component: NewProductComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class AdminRoutingModule { }
+export class AdminRoutingModule {
+}
