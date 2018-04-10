dcdb255863273045a97d7bb9e4e8d2235e7c766b
jaggesher <jaggesher14@gmail.com>
2018-04-10 15:10:32 +0600

    category comoponent added

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -4,12 +4,13 @@ import { CommonModule } from '@angular/common';
 import { AdminRoutingModule } from './admin-routing.module';
 import { NewProductComponent } from './new-product/new-product.component';
 import { FormsModule }   from '@angular/forms';
+import { CategoryComponent } from './category/category.component';
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     AdminRoutingModule
   ],
-  declarations: [NewProductComponent]
+  declarations: [NewProductComponent, CategoryComponent]
 })
 export class AdminModule { }
