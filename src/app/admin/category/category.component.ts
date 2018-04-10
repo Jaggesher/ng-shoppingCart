fa5ce71a10805993cf80f78aec25d61e49c6ed6a
jaggesher <jaggesher14@gmail.com>
2018-04-11 01:12:49 +0600

    service works

--- src/app/admin/category/category.component.ts
+++ src/app/admin/category/category.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { CommonService } from '../../shared/services/common.service';
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
@@ -7,10 +8,13 @@ import { NgForm } from '@angular/forms';
 })
 export class CategoryComponent implements OnInit {
 
-  constructor() { }
+  public AllCategories = [];
+  constructor(private _commonService: CommonService) { }
 
   ngOnInit() {
+    this.AllCategories = this._commonService.getAllCategory();
   }
+  
   addCategory(newCategory: NgForm): void {
     console.log(newCategory.value);
   }
