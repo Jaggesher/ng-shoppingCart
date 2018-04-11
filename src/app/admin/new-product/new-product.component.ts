cf562bec6233f4eb815f3cc84a5a093e2bb35f73
jaggesher <jaggesher14@gmail.com>
2018-04-11 23:40:38 +0600

    product add view and component problems fixed

--- src/app/admin/new-product/new-product.component.ts
+++ src/app/admin/new-product/new-product.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { NgForm, FormGroup, FormControl, Validators } from '@angular/forms';
+import { validateConfig } from '@angular/router/src/config';
+import { CommonService } from '../../shared/services/common.service';
+import { AdminService } from '../Services/admin.service';
 
 @Component({
 	selector: 'app-new-product',
@@ -8,11 +11,59 @@ import { NgForm } from '@angular/forms';
 })
 export class NewProductComponent implements OnInit {
 
-	constructor() { }
+	public allCategory: Object[];
+	public productForm: FormGroup;
+	public customFile: FormControl;
+	public description: FormControl;
+	public category: FormControl;
+	public price: FormControl;
+	public stock: FormControl;
+
+	private createForm() {
+		this.productForm = new FormGroup({
+			customFile: this.customFile,
+			description: this.description,
+			category: this.category,
+			price: this.price,
+			stock: this.stock
+		});
+	}
+
+	private createFormControls() {
+		this.customFile = new FormControl();
+		this.category = new FormControl('', [
+			Validators.required,
+		]);
+		this.description = new FormControl('', [
+			Validators.required,
+			Validators.maxLength(995)
+		]);
+		this.price = new FormControl('', [
+			Validators.required,
+			Validators.min(1),
+			Validators.max(100000000)
+		]);
+		this.stock = new FormControl('', [
+			Validators.required,
+			Validators.min(1),
+			Validators.max(100000)
+		]);
+	}
+
+	private getCategory() {
+		this._commonService.getAllCategory()
+			.subscribe(data => this.allCategory = data, error => console.log(error));
+	}
+
+	constructor(private _commonService: CommonService, private _adminService: AdminService) { }
 
 	ngOnInit() {
+		this.createFormControls();
+		this.createForm();
+		this.getCategory();
 	}
-	addProduct(newProduct: NgForm): void {
-		console.log(newProduct.value);
+
+	onSubmit() {
+		console.log(this.customFile);
 	}
 }
