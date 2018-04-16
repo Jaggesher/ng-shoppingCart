6893a4606faae23c8acb3ce689b3634b0aceb786
jaggesher <jaggesher14@gmail.com>
2018-04-16 22:11:26 +0600

    just commit

--- src/app/shopper/cart/cart.component.ts
+++ src/app/shopper/cart/cart.component.ts
@@ -6,75 +6,75 @@ import { error } from 'protractor';
 import { Product } from '../../shared/models/product';
 
 @Component({
-  selector: 'app-cart',
-  templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+    selector: 'app-cart',
+    templateUrl: './cart.component.html',
+    styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
-  public baseUrl: string;
-  public isRequesting: boolean;
-  public cartForm: FormGroup;
-  public name: FormControl;
-  public address: FormControl;
-  public phone: FormControl;
-  public products: Product[];
-  public total : number;
+    public baseUrl: string;
+    public isRequesting: boolean;
+    public cartForm: FormGroup;
+    public name: FormControl;
+    public address: FormControl;
+    public phone: FormControl;
+    public products: Product[];
+    public total: number;
 
-  private createForm() {
-    this.cartForm = new FormGroup({
-      name: this.name,
-      address: this.address,
-      phone: this.phone,
-    });
-  }
-
-  private createFormControls() {
-    this.name = new FormControl('', [
-      Validators.required,
-      Validators.minLength(3),
-      Validators.maxLength(100)
-    ]);
-    this.phone = new FormControl('', [
-      Validators.required,
-      Validators.minLength(3),
-      Validators.maxLength(100)
-    ]);
-    this.address = new FormControl('', [
-      Validators.required,
-      Validators.maxLength(1000)
-    ]);
-  }
-
-  private getProducts() {
-    this.isRequesting = true;
-    this.total = 0;
-    this._commonService.getAllLocalProductDetails().subscribe(
-      data => {
-        this.products = data;
-        data.forEach(element => {
-          element.inStock = this._commonService.getLocalProductQuantity(element.id);
-          this.total += (element.inStock*element.price);
+    private createForm() {
+        this.cartForm = new FormGroup({
+            name: this.name,
+            address: this.address,
+            phone: this.phone,
         });
-        this.isRequesting = false;
-      },
-      error => {
-        console.log(error);
-        this.isRequesting = false;
-      }
-    );
-  }
+    }
 
-  constructor(private _commonService: CommonService) { }
+    private createFormControls() {
+        this.name = new FormControl('', [
+            Validators.required,
+            Validators.minLength(3),
+            Validators.maxLength(100)
+        ]);
+        this.phone = new FormControl('', [
+            Validators.required,
+            Validators.minLength(3),
+            Validators.maxLength(100)
+        ]);
+        this.address = new FormControl('', [
+            Validators.required,
+            Validators.maxLength(1000)
+        ]);
+    }
 
-  ngOnInit() {
-    this.baseUrl = environment.baseUrl;
-    this.createFormControls();
-    this.createForm();
-    this.getProducts();
-  }
+    private getProducts() {
+        this.isRequesting = true;
+        this.total = 0;
+        this._commonService.getAllLocalProductDetails().subscribe(
+            data => {
+                this.products = data;
+                data.forEach(element => {
+                    element.inStock = this._commonService.getLocalProductQuantity(element.id);
+                    this.total += (element.inStock * element.price);
+                });
+                this.isRequesting = false;
+            },
+            error => {
+                console.log(error);
+                this.isRequesting = false;
+            }
+        );
+    }
 
-  onSubmit() {
+    constructor(private _commonService: CommonService) { }
 
-  }
+    ngOnInit() {
+        this.baseUrl = environment.baseUrl;
+        this.createFormControls();
+        this.createForm();
+        this.getProducts();
+    }
+
+    onSubmit() {
+        console.log(this.cartForm.value);
+    }
 
 }
